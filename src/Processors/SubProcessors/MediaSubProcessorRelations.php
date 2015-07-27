75b1ca3ab658e7aa837a215dc295848e7032d8ca
Deividas Gineika <gineika@users.noreply.github.com>
2015-07-27 14:29:24 +0300

    Fixed relations processor

--- src/Processors/SubProcessors/MediaSubProcessorRelations.php
+++ src/Processors/SubProcessors/MediaSubProcessorRelations.php
@@ -1,84 +1,69 @@
 <?php
 class MediaSubProcessorRelations extends MediaSubProcessor
 {
-	public function process(array $documents, &$context)
-	{
-		$doc = $documents[self::URL_MEDIA];
-		$dom = self::getDOM($doc);
-		$xpath = new DOMXPath($dom);
-
-		Database::delete('mediarelation', ['media_id' => $context->media->id]);
-		$data = [];
-		$lastType = '';
-		foreach ($xpath->query('//h2[starts-with(text(), \'Related\')]/../*') as $node)
-		{
-			if ($node->nodeName == 'h2' and (strpos($node->textContent, 'Related') === false or $node->textContent == 'Related Clubs'))
-			{
-				break;
-			}
-			if ($node->nodeName != 'a')
-			{
-				continue;
-			}
-			$link = $node->attributes->getNamedItem('href')->nodeValue;
-
-			//relation type
-			$malType = strtolower(Strings::removeSpaces($node->previousSibling->textContent));
-			if ($malType == ',')
-			{
-				$type = $lastType;
-			}
-			else
-			{
-				$type = Strings::makeEnum($malType, [
-					'sequel'              => MediaRelation::Sequel,
-					'prequel'             => MediaRelation::Prequel,
-					'side story'          => MediaRelation::SideStory,
-					'parent story'        => MediaRelation::ParentStory,
-					'adaptation'          => MediaRelation::Adaptation,
-					'alternative version' => MediaRelation::AlternativeVersion,
-					'summary'             => MediaRelation::Summary,
-					'character'           => MediaRelation::Character,
-					'spin-off'            => MediaRelation::SpinOff,
-					'alternative setting' => MediaRelation::AlternativeSetting,
-					'other'               => MediaRelation::Other,
-					'full story'          => MediaRelation::FullStory,
-				], null);
-				if ($type === null)
-					throw new BadProcessorDocumentException($doc, 'unknown relation type: ' . $malType);
-				$lastType = $type;
-			}
-
-			//relation id
-			preg_match_all('/([0-9]+)/', $link, $matches);
-			if (!isset($matches[0][0]))
-			{
-				continue;
-			}
-			$mediaMalId = Strings::makeInteger($matches[0][0]);
-
-			//relation media
-			if (strpos($link, '/anime') !== false)
-			{
-				$media = Media::Anime;
-			}
-			elseif (strpos($link, '/manga') !== false)
-			{
-				$media = Media::Manga;
-			}
-			else
-			{
-				continue;
-			}
-
-			$data []= [
-				'media_id' => $context->media->id,
-				'mal_id' => $mediaMalId,
-				'media' => $media,
-				'type' => $type
-			];
-		}
-		Database::insert('mediarelation', $data);
-		$context->relationData = $data;
-	}
+    public function process(array $documents, &$context)
+    {
+        $document = $documents[self::URL_MEDIA];
+        
+        $dom = self::getDOM($document);
+        
+        $xpath = new DOMXPath($dom);
+        
+        Database::delete('mediarelation', [
+            'media_id' => $context->media->id
+        ]);
+        
+        $data = [];
+        
+        foreach ($xpath->query('//table[@class=\'anime_detail_related_anime\']/tr') as $node)
+        {
+            $typeMal = strtolower(Strings::removeSpaces($node->childNodes[0]->textContent));
+            
+            $type = Strings::makeEnum($typeMal, [
+                'adaptation' => MediaRelation::Adaptation,
+                'alternative setting' => MediaRelation::AlternativeSetting,
+                'alternative version' => MediaRelation::AlternativeVersion,
+                'character' => MediaRelation::Character,
+                'full story' => MediaRelation::FullStory,
+                'other' => MediaRelation::Other,
+                'parent story' => MediaRelation::ParentStory,
+                'prequel' => MediaRelation::Prequel,
+                'sequel' => MediaRelation::Sequel,
+                'side story' => MediaRelation::SideStory,
+                'spin-off' => MediaRelation::SpinOff,
+                'summary' => MediaRelation::Summary
+            ], null);
+            
+            if ($type === null) {
+                throw new BadProcessorDocumentException($document, 'unknown relation type: ' . $typeMal);
+            }
+            
+            $links = $node->childNodes[1]->getElementsByTagName('a');
+            
+            foreach ($links as $link) {
+                $link = $link->getAttribute('href');
+                
+                if (preg_match('#^/(anime|manga)/([0-9]+)/#', $link, $matches)) {
+                    $idMal = Strings::makeInteger($matches[2]);
+                    
+                    if ($matches[1] === 'anime') {
+                        $media = Media::Anime;
+                    } elseif ($matches[1] === 'manga') {
+                        $media = Media::Manga;
+                    }
+                    
+                    $data[] = [
+                        'media_id' => $context->media->id,
+                        'mal_id' => $idMal,
+                        'media' => $media,
+                        'type' => $type
+                    ];
+                }
+            }
+        }
+        
+        Database::insert('mediarelation', $data);
+        
+        $context->relationData = $data;
+    }
 }
