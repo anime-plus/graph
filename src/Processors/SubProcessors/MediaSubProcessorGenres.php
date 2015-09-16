fe00468c7e46dde0b23bdb358cd11aa6cb3c03c3
Deividas Gineika <gineika@users.noreply.github.com>
2015-09-16 06:19:57 +0300

    Fixed media processor

--- src/Processors/SubProcessors/MediaSubProcessorGenres.php
+++ src/Processors/SubProcessors/MediaSubProcessorGenres.php
@@ -1,25 +1,37 @@
 <?php
 class MediaSubProcessorGenres extends MediaSubProcessor
 {
-	public function process(array $documents, &$context)
-	{
-		$doc = $documents[self::URL_MEDIA];
-		$dom = self::getDOM($doc);
-		$xpath = new DOMXPath($dom);
-
-		Database::delete('mediagenre', ['media_id' => $context->media->id]);
-		$data = [];
-		foreach ($xpath->query('//span[starts-with(text(), \'Genres\')]/../a') as $node)
-		{
-			preg_match('/=([0-9]+)/', $node->getAttribute('href'), $matches);
-			$genreMalId = Strings::makeInteger($matches[1]);
-			$genreName = Strings::removeSpaces($node->textContent);
-			$data []= [
-				'media_id' => $context->media->id,
-				'mal_id' => $genreMalId,
-				'name' => $genreName
-			];
-		}
-		Database::insert('mediagenre', $data);
-	}
+    public function process(array $documents, &$context)
+    {
+        $document = $documents[self::URL_MEDIA];
+        
+        $dom = self::getDOM($document);
+        
+        $xpath = new DOMXPath($dom);
+        
+        Database::delete(
+            'mediagenre',
+            [
+                'media_id' => $context->media->id
+            ]
+        );
+        
+        $data = [];
+        
+        foreach ($xpath->query('//span[@itemprop = \'genre\']//a') as $node) {
+            preg_match('/=([0-9]+)/', $node->getAttribute('href'), $matches);
+            
+            $genreIdMal = Strings::makeInteger($matches[1]);
+            
+            $genreName = Strings::removeSpaces($node->textContent);
+            
+            $data[] = [
+                'media_id' => $context->media->id,
+                'mal_id' => $genreIdMal,
+                'name' => $genreName
+            ];
+        }
+        
+        Database::insert('mediagenre', $data);
+    }
 }
