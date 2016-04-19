17014976d29a7d0132198b384cc090c01ef67796
gineika <gineika@users.noreply.github.com>
2016-04-19 06:12:36 +0300

    Fixed media processor

--- src/Processors/SubProcessors/AnimeSubProcessorBasic.php
+++ src/Processors/SubProcessors/AnimeSubProcessorBasic.php
@@ -1,34 +1,51 @@
 <?php
 class AnimeSubProcessorBasic extends MediaSubProcessor
 {
-	public function __construct()
-	{
-		parent::__construct(Media::Anime);
-	}
-
-	public function process(array $documents, &$context)
-	{
-		$doc = $documents[self::URL_MEDIA];
-		$dom = self::getDOM($doc);
-		$xpath = new DOMXPath($dom);
-
-		//duration
-		preg_match_all('#([0-9]+)#', self::getNodeValue($xpath, '//span[text() = \'Duration:\']/following-sibling::node()[self::text()]'), $matches);
-		array_reverse($matches);
-		$duration = 0;
-		foreach($matches[0] as $r)
-		{
-			$duration *= 60;
-			$duration += $r;
-		}
-
-		//episode count
-		preg_match_all('#([0-9]+|Unknown)#', self::getNodeValue($xpath, '//span[text() = \'Episodes:\']/following-sibling::node()[self::text()]'), $matches);
-		$episodeCount = Strings::makeInteger($matches[0][0]);
-
-		$media = &$context->media;
-		$media->duration = $duration;
-		$media->episodes = $episodeCount;
-		R::store($media);
-	}
+    public function __construct()
+    {
+        parent::__construct(Media::Anime);
+    }
+    
+    public function process(array $documents, &$context)
+    {
+        $document = $documents[self::URL_MEDIA];
+        
+        $dom = self::getDOM($document);
+        
+        $xpath = new DOMXPath($dom);
+        
+        preg_match_all('#([0-9]+) (hr|min|sec)#', self::getNodeValue($xpath, '//span[text() = \'Duration:\']/following-sibling::node()[self::text()]'), $matches);
+        
+        array_reverse($matches);
+        
+        $duration = 0;
+        
+        foreach ($matches[1] as $key => $value) {
+            switch ($matches[2][$key]) {
+                case 'hr':
+                    $duration += $value * 60;
+                    
+                    break;
+                case 'min':
+                    $duration += $value;
+                    
+                    break;
+                case 'sec':
+                    $duration++;
+                    
+                    break;
+            }
+        }
+        
+        preg_match_all('#([0-9]+|Unknown)#', self::getNodeValue($xpath, '//span[text() = \'Episodes:\']/following-sibling::node()[self::text()]'), $matches);
+        
+        $episodes = Strings::makeInteger($matches[0][0]);
+        
+        $media = &$context->media;
+        
+        $media->duration = $duration;
+        $media->episodes = $episodes;
+        
+        R::store($media);
+    }
 }
