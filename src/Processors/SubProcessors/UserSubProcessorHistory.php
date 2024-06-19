f29ff1c4c57b10f24c6a6d0649424832eceaf114
gineika <gineika@users.noreply.github.com>
2024-06-19 14:28:29 +0300

    separated anime/manga history parsing

--- src/Processors/SubProcessors/UserSubProcessorHistory.php
+++ src/Processors/SubProcessors/UserSubProcessorHistory.php
@@ -1,125 +1,146 @@
 <?php
 class UserSubProcessorHistory extends UserSubProcessor
 {
-    const URL_HISTORY = 0;
+    const URL_ANIME_HISTORY = 0;
+    const URL_MANGA_HISTORY = 1;
 
     public function getURLs($username)
     {
         return [
-            self::URL_HISTORY => 'https://myanimelist.net/history/' . $username
+            self::URL_ANIME_HISTORY => 'https://myanimelist.net/history/' . $username . '/anime',
+            self::URL_MANGA_HISTORY => 'https://myanimelist.net/history/' . $username . '/manga',
         ];
     }
 
     public function process(array $documents, &$context)
     {
-        $document = $documents[self::URL_HISTORY];
-
-        $dom = self::getDOM($document);
-
-        $xpath = new DOMXPath($dom);
-
         Database::delete('userhistory', [
-            'user_id' => $context->user->id
+            'user_id' => $context->user->id,
         ]);
 
         $data = [];
 
-        foreach ($xpath->query('//table//td[@class = \'borderClass\']/..') as $node) {
-            $link = $node->childNodes->item(0)->childNodes->item(0)->getAttribute('href');
+        foreach ([self::URL_ANIME_HISTORY, self::URL_MANGA_HISTORY] as $k)
+        {
+            $document = $documents[$k];
 
-            if (!preg_match('#([0-9]+)$#', $link, $matches)) {
-                continue;
-            }
+            $dom = self::getDOM($document);
 
-            $media = strpos($link, 'manga') !== false ? Media::Manga : Media::Anime;
+            $xpath = new DOMXPath($dom);
 
-            $idMal = intval($matches[0]);
+            foreach ($xpath->query('//table//td[@class = \'borderClass\']/..') as $node)
+            {
+                $link = $node->childNodes->item(0)->childNodes->item(0)->getAttribute('href');
 
-            $progress = Strings::makeInteger($node->childNodes->item(0)->getElementsByTagName('strong')->item(0)->nodeValue);
+                if (!preg_match('#([0-9]+)$#', $link, $matches))
+                {
+                    continue;
+                }
 
-            if (isset($document->headers['Date'])) {
+                $media = strpos($link, 'manga') !== false ? Media::Manga : Media::Anime;
+
+                $idMal = intval($matches[0]);
+
+                $progress = Strings::makeInteger($node->childNodes->item(0)->getElementsByTagName('strong')->item(0)->nodeValue);
+
+                $time = time();
+
+                if (isset($document->headers['Date']))
+                {
+                    date_default_timezone_set('UTC');
+
+                    $time = strtotime($document->headers['Date']);
+                }
+
+                date_default_timezone_set('America/Los_Angeles');
+
+                $y = date('Y', $time);
+
+                $m = date('m', $time);
+
+                $d = date('d', $time);
+
+                $h = date('H', $time);
+
+                $i = date('i', $time);
+
+                $s = date('s', $time);
+
+                $date = $node->childNodes->item(1)->nodeValue;
+
+                if (preg_match('#([0-9]+) seconds? ago#i', $date, $matches))
+                {
+                    $s -= intval($matches[1]);
+                }
+                elseif (preg_match('#([0-9]+) minutes? ago#i', $date, $matches))
+                {
+                    $s -= intval($matches[1]) * 60;
+                }
+                elseif (preg_match('#([0-9]+) hours? ago#i', $date, $matches))
+                {
+                    $i -= intval($matches[1]) * 60;
+                }
+                elseif (preg_match('#Today, ([0-9]{1,2}):([0-9]{2}) (AM|PM)#i', $date, $matches))
+                {
+                    $h = intval($matches[1]);
+
+                    $i = intval($matches[2]);
+
+                    $h += $matches[3] === 'PM' && $h !== 12 ? 12 : 0;
+                }
+                elseif (preg_match('#Yesterday, ([0-9]{1,2}):([0-9]{2}) (AM|PM)#i', $date, $matches))
+                {
+                    $h = intval($matches[1]);
+
+                    $i = intval($matches[2]);
+
+                    $h += $matches[3] === 'PM' && $h !== 12 ? 12 : 0;
+
+                    $h -= 24;
+                }
+                elseif (preg_match('#([a-z]+) ([0-9]{1,2}), ([0-9]{1,2}):([0-9]{2}) (AM|PM)#i', $date, $matches))
+                {
+                    $m = date_parse($matches[1]);
+
+                    $m = intval($m['month']);
+
+                    $d = intval($matches[2]);
+
+                    $h = intval($matches[3]);
+
+                    $i = intval($matches[4]);
+
+                    $h += $matches[5] === 'PM' && $h !== 12 ? 12 : 0;
+                }
+                elseif (preg_match('#([a-z]+) ([0-9]{1,2}), ([0-9]{4}) ([0-9]{1,2}):([0-9]{2}) (AM|PM)#i', $date, $matches))
+                {
+                    $m = date_parse($matches[1]);
+
+                    $m = intval($m['month']);
+
+                    $d = intval($matches[2]);
+
+                    $y = intval($matches[3]);
+
+                    $h = intval($matches[4]);
+
+                    $i = intval($matches[5]);
+
+                    $h += $matches[6] === 'PM' && $h !== 12 ? 12 : 0;
+                }
+
+                $timestamp = mktime($h, $i, $s, $m, $d, $y);
+
                 date_default_timezone_set('UTC');
 
-                $now = strtotime($document->headers['Date']);
-            } else {
-                $now = time();
+                $data[] = [
+                    'user_id' => $context->user->id,
+                    'mal_id' => $idMal,
+                    'media' => $media,
+                    'progress' => $progress,
+                    'timestamp' => date('Y-m-d H:i:s', $timestamp),
+                ];
             }
-
-            date_default_timezone_set('America/Los_Angeles');
-
-            $year = date('Y', $now);
-
-            $month = date('m', $now);
-
-            $day = date('d', $now);
-
-            $hour = date('H', $now);
-
-            $minute = date('i', $now);
-
-            $second = date('s', $now);
-
-            $date = $node->childNodes->item(1)->nodeValue;
-
-            if (preg_match('#([0-9]+) seconds? ago#i', $date, $matches)) {
-                $second -= intval($matches[1]);
-            } elseif (preg_match('#([0-9]+) minutes? ago#i', $date, $matches)) {
-                $second -= intval($matches[1]) * 60;
-            } elseif (preg_match('#([0-9]+) hours? ago#i', $date, $matches)) {
-                $minute -= intval($matches[1]) * 60;
-            } elseif (preg_match('#Today, ([0-9]{1,2}):([0-9]{2}) (AM|PM)#i', $date, $matches)) {
-                $hour = intval($matches[1]);
-
-                $minute = intval($matches[2]);
-
-                $hour += $matches[3] === 'PM' && $hour !== 12 ? 12 : 0;
-            } elseif (preg_match('#Yesterday, ([0-9]{1,2}):([0-9]{2}) (AM|PM)#i', $date, $matches)) {
-                $hour = intval($matches[1]);
-
-                $minute = intval($matches[2]);
-
-                $hour += $matches[3] === 'PM' && $hour !== 12 ? 12 : 0;
-
-                $hour -= 24;
-            } elseif (preg_match('#([a-z]+) ([0-9]{1,2}), ([0-9]{1,2}):([0-9]{2}) (AM|PM)#i', $date, $matches)) {
-                $month = date_parse($matches[1]);
-
-                $month = intval($month['month']);
-
-                $day = intval($matches[2]);
-
-                $hour = intval($matches[3]);
-
-                $minute = intval($matches[4]);
-
-                $hour += $matches[5] === 'PM' && $hour !== 12 ? 12 : 0;
-            } elseif (preg_match('#([a-z]+) ([0-9]{1,2}), ([0-9]{4}) ([0-9]{1,2}):([0-9]{2}) (AM|PM)#i', $date, $matches)) {
-                $month = date_parse($matches[1]);
-
-                $month = intval($month['month']);
-
-                $day = intval($matches[2]);
-
-                $year = intval($matches[3]);
-
-                $hour = intval($matches[4]);
-
-                $minute = intval($matches[5]);
-
-                $hour += $matches[6] === 'PM' && $hour !== 12 ? 12 : 0;
-            }
-
-            $timestamp = mktime($hour, $minute, $second, $month, $day, $year);
-
-            date_default_timezone_set('UTC');
-
-            $data[] = [
-                'user_id' => $context->user->id,
-                'mal_id' => $idMal,
-                'media' => $media,
-                'progress' => $progress,
-                'timestamp' => date('Y-m-d H:i:s', $timestamp)
-            ];
         }
 
         Database::insert('userhistory', $data);
